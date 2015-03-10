520311493e38d696f3fd3d97118873c468f2ebd7
Bill Cary <bill.cary@gmail.com>
2015-03-10 16:17:04 -0400

    Updates to ui.R and server.R to match project objective (concrete compressive strength prediction).

--- project/server.R
+++ project/server.R
@@ -1,6 +1,55 @@
-library(shiny)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Import required libraries
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(shiny)
+suppressMessages(library(ggplot2))       # General plotting functions
+suppressMessages(library(fmsb))          # Radar/spider plotting
+suppressMessages(library(doParallel))    # Parallel processing
+suppressMessages(library(caret))         # Machine learning
+suppressMessages(library(AppliedPredictiveModeling)) # Concrete data set
+suppressMessages(library(randomForest))  # Machine learning
+suppressMessages(library(Metrics))       # Performance measures
 diabetesRisk <- function(glucose) glucose / 200
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Read the data into R
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+data <- data("concrete")
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Holdout 20% of data for testing estimates
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(107)
+inTrain <- createDataPartition(y = concrete$CompressiveStrength,
+                               p = 0.80,
+                               list = FALSE)
+
+training <- concrete[inTrain,]
+testing <- concrete[-inTrain,]
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Train the model
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(300)
+
+model.rf <- randomForest(CompressiveStrength ~ .
+                         ,data = training
+                         ,xtest = testing[, 1:8]
+                         ,ytest = testing$CompressiveStrength
+                         ,mtry = 6
+                         ,ntree = 750
+                         ,importance = TRUE)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Performance plots
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+predictions.rf <- predict(model.rf, concrete, predict.all = TRUE)
+residuals.rf <- concrete$CompressiveStrength - predictions.rf
+plot(concrete$CompressiveStrength, residuals.rf)
+plot(predictions.rf, residuals.rf)
+plot(concrete$CompressiveStrength, predictions.rf)
+varImpPlot(model.rf, type = 1, scale = TRUE)
 
 shinyServer(
   function(input, output) {
--- project/ui.R
+++ project/ui.R
@@ -1,22 +1,36 @@
 library(shiny)
 
 # Define UI for dataset viewer application
-shinyUI(
-  pageWithSidebar(
-    # Application title
-    headerPanel("Diabetes prediction"),
-  
-    sidebarPanel(
-      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
-      submitButton('Submit')
-    ),
-    mainPanel(
-        h3('Results of prediction'),
-        h4('You entered'),
-        verbatimTextOutput("inputValue"),
-        h4('Which resulted in a prediction of '),
-        verbatimTextOutput("prediction")
-    )
-  )
-)
+shinyUI(navbarPage("Predicting compressive strength of concrete mixtures",
+                   inverse = TRUE, collapsable = FALSE, fluid = TRUE, responsive = TRUE,
+                   tabPanel("About App",
+                            fluidRow(includeHTML("AboutApp.html"))),
+                   tabPanel("Predict", # Sidebar with a slider input for number of bins
+                            sidebarLayout(
+                                    sidebarPanel("Use sliders to choose the amount of each concrete component",
+                                                 sliderInput("cement","Cement (Kg/m^3)", 17, 27, 20, 0.5),
+                                                 sliderInput("age","Concrete Age (days)", 50, 80, 50, 0.5),
+                                                 sliderInput("fine.aggregate","Fine Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("coarse.aggregate","Coarse Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("super.plasticizer","Super Plasticizer (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("water","Water (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("fly.ash","Fly Ash (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("blast.furnace.slag","Blast Furnace Slag (Kg/m^3)", 50, 80, 50, 0.5),),
+                                    mainPanel(            
+                                            verbatimTextOutput("predict"),
+                                            textOutput("text1"),
+                                            plotOutput("plot1")))),
+                   navbarMenu("Model Performance",
+                              tabPanel("Actual Strength vs. Residuals",   
+                                       verbatimTextOutput("summary")),
+                              tabPanel("Predicted Strength vs. Residuals",   
+                                       plotOutput("plot1")),
+                              tabPanel("Actual Strength vs. Predicted Strength",   
+                                       plotOutput("plot2")),
+                              tabPanel("Variable Importance",   
+                                       plotOutput("plot3"))),
+                   tabPanel("About Stackloss", 
+                            fluidRow(includeHTML("AboutStackloss.html")))        
+                   
+))
 
