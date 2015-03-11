60f7add081d5b21a32072273617b8a57d3e04391
Bill Cary <bill.cary@gmail.com>
2015-03-11 17:03:00 -0400

    Misc updates

--- project/server.R
+++ project/server.R
@@ -46,14 +46,77 @@ model.rf <- randomForest(CompressiveStrength ~ .
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 predictions.rf <- predict(model.rf, concrete, predict.all = TRUE)
 residuals.rf <- concrete$CompressiveStrength - predictions.rf
-plot(concrete$CompressiveStrength, residuals.rf)
-plot(predictions.rf, residuals.rf)
-plot(concrete$CompressiveStrength, predictions.rf)
-varImpPlot(model.rf, type = 1, scale = TRUE)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Provide initial values for the user input sliders
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sample <- data.frame(Cement = 293.0,
+                     BlastFurnaceSlag = 6.5,
+                     FlyAsh = 94.0,
+                     Water = 200.0,
+                     Superplasticizer = 16.1,
+                     CoarseAggregate = 855.0,
+                     FineAggregate = 800.0,
+                     Age = 14.0)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ShinyServer function
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 shinyServer(
-  function(input, output) {
-    output$inputValue <- renderPrint({input$glucose})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-  }
+        function(input, output) {
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Overall Model Performance plots
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                
+                # Plot residuals vs. actual compressive strength
+                output$plot1 <- renderPlot({
+                        plot(concrete$CompressiveStrength, residuals.rf)
+                })
+                
+                # Plot residuals vs. predicted strength
+                output$plot2 <- renderPlot({
+                        plot(predictions.rf, residuals.rf)
+                })
+                
+                # Plot predictions vs. actual compressive strength
+                output$plot3 <- renderPlot({
+                        plot(concrete$CompressiveStrength, predictions.rf)
+                })
+                
+                # Plot variable importance
+                output$plot4 <- renderPlot({
+                        varImpPlot(model.rf, type = 1, scale = TRUE)
+                })
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Predict compressive strength using example parameters
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                sample <- data.frame(Cement = {input$cement},
+                                     BlastFurnaceSlag = {input$blast.furnace.slag},
+                                     FlyAsh = {input$fly.ash},
+                                     Water = {input$water},
+                                     Superplasticizer = {input$super.plasticizer},
+                                     CoarseAggregate = {input$coarse.aggregate},
+                                     FineAggregate = {input$fine.aggregate},
+                                     Age = {input$age})
+                
+                predict.sample <- predict(model.rf, sample, predict.all = TRUE)
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Render outputs to screen
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Render output prediction and confidence interval
+                
+                # Render user input values
+                
+                # Render radar plot of user input values
+                
+                # Plot variable importance
+                output$plot4 <- renderPlot({
+                        varImpPlot(model.rf, type = 1, scale = TRUE)
+                })
+                output$inputValue <- renderPrint({input$glucose})
+                output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+        }
 )
