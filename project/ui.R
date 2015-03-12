a01660ee5d5105cc3c2572071963f60dd96aedda
Bill Cary <bill.cary@gmail.com>
2015-03-12 17:04:12 -0400

    Started adding real code and verifying correct function of application.

--- project/ui.R
+++ project/ui.R
@@ -4,7 +4,7 @@ library(shiny)
 shinyUI(fluidPage(
         
         # Application title
-        titlePanel("Tabsets"),
+        titlePanel("Predicting compressive strength of concrete mixtures"),
         
         # Sidebar with controls to select the random distribution type
         # and number of observations to generate. Note the use of the
@@ -22,16 +22,30 @@ shinyUI(fluidPage(
                                     "Number of observations:", 
                                     value = 500,
                                     min = 1, 
-                                    max = 1000)
+                                    max = 1000),
+                        
+                        br(),
+                        
+                        sliderInput("cement","Cement (Kg/m^3)", 17, 27, 20, 0.5),
+                        sliderInput("age","Concrete Age (days)", 50, 80, 50, 0.5),
+                        sliderInput("fine.aggregate","Fine Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                        sliderInput("coarse.aggregate","Coarse Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                        sliderInput("super.plasticizer","Super Plasticizer (Kg/m^3)", 50, 80, 50, 0.5),
+                        sliderInput("water","Water (Kg/m^3)", 50, 80, 50, 0.5),
+                        sliderInput("fly.ash","Fly Ash (Kg/m^3)", 50, 80, 50, 0.5),
+                        sliderInput("blast.furnace.slag","Blast Furnace Slag (Kg/m^3)", 50, 80, 50, 0.5)
                 ),
                 
                 # Show a tabset that includes a plot, summary, and table view
                 # of the generated distribution
                 mainPanel(
                         tabsetPanel(type = "tabs", 
-                                    tabPanel("Plot", plotOutput("plot")), 
-                                    tabPanel("Summary", verbatimTextOutput("summary")), 
-                                    tabPanel("Table", tableOutput("table"))
+                                    tabPanel("Prediction", plotOutput("plot")),
+                                    tabPanel("Residuals vs. Actual Strength", plotOutput("plot")), 
+                                    tabPanel("Residuals vs. Predicted Strength", verbatimTextOutput("summary")), 
+                                    tabPanel("Predicted Strength vs. Actual Strength", tableOutput("table")),
+                                    tabPanel("Variable Importance", tableOutput("table")),
+                                    tabPanel("About Application", plotOutput("plot"))
                         )
                 )
         )
