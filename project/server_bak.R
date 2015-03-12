c659370599d163936eebd78a4165e7d4ae6106f3
Bill Cary <bill.cary@gmail.com>
2015-03-12 16:49:43 -0400

    Backup files with "real" project code and replace server.R and ui.R files with samples from RStudio to start pasting in real code piece by piece.  Wanted to start from a working codebase and see where I break it.

---
+++ project/server_bak.R
@@ -0,0 +1,171 @@
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Import required libraries
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(shiny)
+suppressMessages(library(fmsb))          # Radar/spider plotting
+suppressMessages(library(caret))         # Machine learning
+suppressMessages(library(AppliedPredictiveModeling)) # Concrete data set
+suppressMessages(library(randomForest))  # Machine learning
+
+cat('hello')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Read the data into R
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+data("concrete")
+
+data <- concrete
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Holdout 20% of data for testing estimates
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(107)
+inTrain <- createDataPartition(y = concrete$CompressiveStrength,
+                               p = 0.80,
+                               list = FALSE)
+
+training <- concrete[inTrain,]
+testing <- concrete[-inTrain,]
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Train the model
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(300)
+
+model.rf <- randomForest(CompressiveStrength ~ .
+                         ,data = training
+                         ,xtest = testing[, 1:8]
+                         ,ytest = testing$CompressiveStrength
+                         ,mtry = 6
+                         ,ntree = 750
+                         ,importance = TRUE
+                         ,keep.forest = TRUE)
+
+cat(summary(model.rf))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Performance plots
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+predictions.rf <- predict(model.rf, concrete, predict.all = TRUE)
+
+cat(predictions.rf$aggregate)
+
+residuals.rf <- concrete$CompressiveStrength - predictions.rf$aggregate
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ShinyServer function
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+shinyServer(
+        function(input, output) {
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Provide initial values for the user input sliders
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                sample <- data.frame(Cement = 293.0,
+                                     BlastFurnaceSlag = 6.5,
+                                     FlyAsh = 94.0,
+                                     Water = 200.0,
+                                     Superplasticizer = 16.1,
+                                     CoarseAggregate = 855.0,
+                                     FineAggregate = 800.0,
+                                     Age = 14.0)
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Overall Model Performance plots
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                
+                # Plot residuals vs. actual compressive strength
+                output$plot1 <- renderPlot({
+                        plot(concrete$CompressiveStrength, residuals.rf)
+                })
+                
+                # Plot residuals vs. predicted strength
+                output$plot2 <- renderPlot({
+                        plot(predictions.rf, residuals.rf)
+                })
+                
+                # Plot predictions vs. actual compressive strength
+                output$plot3 <- renderPlot({
+                        plot(concrete$CompressiveStrength, predictions.rf)
+                })
+                
+                # Plot variable importance
+                output$plot4 <- renderPlot({
+                        varImpPlot(model.rf, type = 1, scale = TRUE)
+                })
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Predict compressive strength using example parameters
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                sample <- data.frame(Cement = {input$cement},
+                                     BlastFurnaceSlag = {input$blast.furnace.slag},
+                                     FlyAsh = {input$fly.ash},
+                                     Water = {input$water},
+                                     Superplasticizer = {input$super.plasticizer},
+                                     CoarseAggregate = {input$coarse.aggregate},
+                                     FineAggregate = {input$fine.aggregate},
+                                     Age = {input$age})
+                
+                predict.sample <- predict(model.rf, sample, predict.all = TRUE)
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Calculate the 95% confidence interval for the prediction based
+                # on the user's input values.
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                predict.sample.int <- apply( predict.sample$individual, 1, function(x) {
+                        c( quantile(x, c(0.025)),
+                           mean(x),
+                           quantile(x, c(0.975)) )
+                })
+                
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Render outputs to screen
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Render output prediction and confidence interval
+                output$prediction.text <- renderText({paste("Based on the chosen parameters,
+                                                  the predicted compressive strength
+                                                  is ", predict.sample.int[2], ".")
+                })
+                
+                output$prediction.ci <- renderText({paste("The confidence interval
+                                                          for the prediction is ",
+                                                          predict.sample.int[1],
+                                                          " to ", 
+                                                          predict.sample.int[3], ".")
+                })
+                
+                # Render user input values
+                output$input.vals <- renderText({paste("The selected input 
+                                                       values are as follows: ",
+                                                       sample)
+                })
+                
+                
+                # Render radar plot of user input values
+                output$plot5 <- renderPlot({
+                        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                        # Set up a dataframe containing the maximum and minimum
+                        # column values as rows 1 and 2, and the values the users
+                        # inputs for prediction as row 3.  This format is 
+                        # required by radarchart (below) to format the plot properly.
+                        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                        plot.data <- rbind(colMaxs(as.matrix(data)),
+                                           colMins(as.matrix(data)),
+                                           sample)
+                        
+                        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                        # Plot the user-input values on a radar chart.
+                        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                        radarchart(plot.data,
+                                   maxmin = TRUE,
+                                   centerzero = TRUE,
+                                   pfcol = c(5),
+                                   pcol = c(4),
+                                   pty = c(16),
+                                   plty = c(7),
+                                   cglcol = 'red',
+                                   title = 'Current Concrete Composition Selection')
+                })
+        })
+)
---
+++ project/ui_bak.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(navbarPage("Predicting compressive strength of concrete mixtures",
+                   inverse = TRUE, collapsable = FALSE, fluid = TRUE, responsive = TRUE,
+                   tabPanel("About App",
+                            #fluidRow(includeHTML("AboutApp.html"))
+                   ),
+                   tabPanel("Predict", # Sidebar with a slider input for number of bins
+                            sidebarLayout(
+                                    sidebarPanel("Use sliders to choose the amount of each concrete component",
+                                                 sliderInput("cement","Cement (Kg/m^3)", 17, 27, 20, 0.5),
+                                                 sliderInput("age","Concrete Age (days)", 50, 80, 50, 0.5),
+                                                 sliderInput("fine.aggregate","Fine Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("coarse.aggregate","Coarse Aggregate (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("super.plasticizer","Super Plasticizer (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("water","Water (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("fly.ash","Fly Ash (Kg/m^3)", 50, 80, 50, 0.5),
+                                                 sliderInput("blast.furnace.slag","Blast Furnace Slag (Kg/m^3)", 50, 80, 50, 0.5),),
+                                    mainPanel(            
+                                            textOutput("prediction.text"),
+                                            textOutput("prediction.ci"),
+                                            textOutput("input.vals"),
+                                            plotOutput("plot5")))),
+                   navbarMenu("Model Performance",
+                              tabPanel("Residuals vs. Actual Strength",   
+                                       plotOutput("plot1")),
+                              tabPanel("Residuals vs. Predicted Strength",   
+                                       plotOutput("plot2")),
+                              tabPanel("Predicted Strength vs. Actual Strength",   
+                                       plotOutput("plot3")),
+                              tabPanel("Variable Importance",   
+                                       plotOutput("plot4")),
+                              tabPanel("About Stackloss", 
+                                       #fluidRow(includeHTML("AboutStackloss.html"))
+                              )        
+                              
+                   )
+)
+)
+
